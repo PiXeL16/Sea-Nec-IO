eae4c066b2cb56a915b6e3987ae7f4da0a03f10a
Chris Jimenez <cjimenez16@gmail.com>
2015-12-14 23:02:10 -0600

    Add incomming call receiver class

---
+++ app/src/main/java/com/greenpixels/seanecio/general_classes/Constants.java
@@ -0,0 +1,15 @@
+package com.greenpixels.seanecio.general_classes;
+
+/**
+ * Created by chris on 12/14/15.
+ * Constant values
+ */
+public class Constants {
+
+    private static final String FIREBASE_URL = "https://seanecio.firebaseio.com/";
+
+
+    public static String getFirebaseUrl() {
+        return FIREBASE_URL;
+    }
+}
