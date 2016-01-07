4be2db53d920867313d5d88edbde6c4faeb5b82c
Christopher Jimenez <cjimenez16@gmail.com>
2016-01-07 16:45:40 -0600

    Improve strings

--- app/src/main/java/com/greenpixels/seanecio/activities/ReportPhoneNumberActivity.java
+++ app/src/main/java/com/greenpixels/seanecio/activities/ReportPhoneNumberActivity.java
@@ -2,7 +2,6 @@ package com.greenpixels.seanecio.activities;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -68,7 +67,7 @@ public class ReportPhoneNumberActivity extends MvpViewStateActivity<ReportPhoneN
 
         _component.inject(this);
     }
-    
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
